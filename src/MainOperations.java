0ba6a573e01d2968dadf34e93e9c3ede3ee55464
Joseph <jaworks.co@gmail.com>
2017-12-24 17:05:29 -0700

    Commit my runnable map project

---
+++ src/MainOperations.java
@@ -0,0 +1,67 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+public class MainOperations {
+	
+	private static Map<Integer, RunnableClass> myMap = new HashMap<>();
+	private ScheduledThreadPoolExecutor scheduledExecutor = (ScheduledThreadPoolExecutor)Executors.newScheduledThreadPool(4, new ThreadFactory(){
+        public Thread newThread(Runnable r) {
+            Thread t = Executors.defaultThreadFactory().newThread(r);
+            t.setDaemon(true);
+            return t;
+        }
+    });
+	
+	public MainOperations() {
+		
+		RunnableClass first = new RunnableClass("FIRST", 0);
+		RunnableClass second = new RunnableClass("SECOND", 1);
+		RunnableClass third = new RunnableClass("THIRD", 2);
+		RunnableClass fourth = new RunnableClass("FOURTH", 3);
+		
+		scheduledExecutor.scheduleAtFixedRate(first, 10, 10, TimeUnit.SECONDS);
+		scheduledExecutor.scheduleAtFixedRate(second, 10, 10, TimeUnit.SECONDS);
+		scheduledExecutor.scheduleAtFixedRate(third, 10, 10, TimeUnit.SECONDS);
+		scheduledExecutor.scheduleAtFixedRate(fourth, 10, 10, TimeUnit.SECONDS);
+		
+		myMap.put(0, first);
+		myMap.put(1, second);
+		myMap.put(2, third);
+		myMap.put(3, fourth);	
+	}
+	
+	public RunnableClass getRunnable(Integer i) {
+		synchronized (myMap) {
+			return myMap.get(i);
+		}
+	}
+
+	//My main
+	public static void main(String[] args) {
+		MainOperations starterClass = new MainOperations();
+		while(true) {
+			try {
+					Thread.sleep(10000L);
+				
+					for(int index=0; index<4; index++) {
+					
+						RunnableClass x = starterClass.getRunnable(index);
+						synchronized (x) {
+							System.out.println("MAIN GET -- NAME: " + x.getName());
+							System.out.println("MAIN GET -- CURRENTMESSAGE: " + x.getMessage());
+						}
+					}
+				} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				}
+			}	
+		}
+	}
+
+
+
---
+++ src/RunnableClass.java
@@ -0,0 +1,32 @@
+
+public class RunnableClass implements Runnable {
+
+	private String name;
+	private Integer currentMessage = 0;
+	private int mapKey;
+	
+	public RunnableClass(String n, int key) {
+		name = n;
+		mapKey = key;
+	}
+	
+	public Integer getMessage() {
+		return new Integer(currentMessage);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+	@Override
+	public void run() {
+		
+		synchronized (currentMessage) {
+			currentMessage++;
+			System.out.println("THREAD NAME: " + name);
+			System.out.println("THREAD MAP KEY: " + mapKey);
+			System.out.println("THREAD " + name + " CURRENT MESSAGE: " + currentMessage);	
+		}
+	}
+}
